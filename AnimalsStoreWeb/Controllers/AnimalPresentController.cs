1d0aae24732ffe0bbb438d50f25f8db65963df33
Рамазан <sh.ramazan47@gmail.com>
2021-12-06 17:04:02 +0300

    хуй

---
+++ AnimalsStoreWeb/Controllers/AnimalPresentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnimalsStoreWeb.Controllers
+{
+    public class AnimalPresentController : Controller
+    {
+        // GET: AnimalPresent
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AnimalsStoreWeb/Controllers/AnimalStoreController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnimalsStoreWeb.Controllers
+{
+    public class AnimalStoreController : Controller
+    {
+        // GET: AnimalStore
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
